daf8e9a6f376f502839c15e9675cde72d6157d94
Yago Gomes <yagogomesc@gmail.com>
2021-05-19 08:33:03 -0300

    Remove commented code

--- src/App.tsx
+++ src/App.tsx
@@ -5,44 +5,8 @@ import "./styles/content.scss";
 import "./styles/global.scss";
 import "./styles/sidebar.scss";
 
-// interface GenreResponseProps {
-//   id: number;
-//   name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
-//   title: string;
-// }
-
-// interface MovieProps {
-//   imdbID: string;
-//   Title: string;
-//   Poster: string;
-//   Ratings: Array<{
-//     Source: string;
-//     Value: string;
-//   }>;
-//   Runtime: string;
-// }
-
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
-
-  // const [movies, setMovies] = useState<MovieProps[]>([]);
-  // const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>(
-  //   {} as GenreResponseProps
-  // );
-
-  // useEffect(() => {
-  //   api
-  //     .get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`)
-  //     .then((response) => {
-  //       setMovies(response.data);
-  //     });
-
-  //   api
-  //     .get<GenreResponseProps>(`genres/${selectedGenreId}`)
-  //     .then((response) => {
-  //       setSelectedGenre(response.data);
-  //     });
-  // }, [selectedGenreId]);
 
   return (
     <div style={{ display: "flex", flexDirection: "row" }}>
@@ -51,27 +15,6 @@ export function App() {
         setSelectedGenreId={setSelectedGenreId}
       />
       <Content selectedGenreId={selectedGenreId} />
-      {/* <div className="container">
-        <header>
-          <span className="category">
-            Categoria:<span> {selectedGenre.title}</span>
-          </span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map((movie) => (
-              <MovieCard
-                key={movie.imdbID}
-                title={movie.Title}
-                poster={movie.Poster}
-                runtime={movie.Runtime}
-                rating={movie.Ratings[0].Value}
-              />
-            ))}
-          </div>
-        </main>
-      </div> */}
     </div>
   );
 }
